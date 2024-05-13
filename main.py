a600efd945554933d8f8af1608241ba3b8e0ef9c
Boy Sugijakko <boysugijakko2000@gmail.com>
2024-05-13 15:49:54 +0700

    INIT

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import os
 from tqdm import tqdm
+from openpyxl import load_workbook
+
 
 def is_currency(value):
     if pd.isna(value) or value == '':
@@ -85,9 +87,11 @@ def extract_transactions(dataframe):
             transactions.append(transaction)
             break
 
-        # New Transaction
-        if(not pd.isna(row['amount'])) and (pd.isna(row['prev_amount'])):
+        if (row['desc'] == 'SALDO AWAL'):
+            continue
 
+        # New Transaction
+        if(not pd.isna(row['amount'])) and ((pd.isna(row['prev_amount'])) or row['amount'] != row['prev_amount']):
             # Save previous transaction
             if temp:
                 transaction = {
@@ -122,6 +126,31 @@ def extract_transactions(dataframe):
     return transaction_dataframe
 
 
+def calculate_balance(dataframe):
+
+    dataframe['balance'] = init_balance
+    # Iterate over rows
+    for index, row in dataframe.iterrows():
+        # If transaction type is 'DB', subtract amount from balance
+        if row['transaction_type'] == 'DB':
+            if index == 0:
+                # For the first row, subtract amount from init_balance
+                dataframe.at[index, 'balance'] -= row['amount']
+            else:
+                # For subsequent rows, subtract amount from the previous row's balance
+                dataframe.at[index, 'balance'] = dataframe.at[index - 1, 'balance'] - row['amount']
+        # If transaction type is 'CR', add amount to balance
+        elif row['transaction_type'] == 'CR':
+            if index == 0:
+                # For the first row, add amount to init_balance
+                dataframe.at[index, 'balance'] += row['amount']
+            else:
+                # For subsequent rows, add amount to the previous row's balance
+                dataframe.at[index, 'balance'] = dataframe.at[index - 1, 'balance'] + row['amount']
+
+    return dataframe
+
+
 def save_to_excel(dataframe, output_filename):
 
     if os.path.isfile(output_filename):
@@ -129,7 +158,7 @@ def save_to_excel(dataframe, output_filename):
     else:
         writer = pd.ExcelWriter(output_filename, engine="openpyxl")
 
-    transaction_dataframe.to_excel(writer, sheet_name=periode, index=False)
+    dataframe.to_excel(writer, sheet_name=periode, index=False)
 
     workbook = writer.book
     worksheet = writer.sheets[periode]
@@ -143,8 +172,8 @@ def save_to_excel(dataframe, output_filename):
     # Autofit column width
     for column_cells in worksheet.columns:
         max_length = max(len(str(cell.value)) for cell in column_cells)
-        if column_cells[0].column in ['E', 'G']:
-            worksheet.column_dimensions[column_cells[0].column_letter].width = max_length + 50
+        if str(column_cells[0].column) in ['5', '7']:
+            worksheet.column_dimensions[column_cells[0].column_letter].width = max_length + 10
         else:
             worksheet.column_dimensions[column_cells[0].column_letter].width = max_length + 2
 
@@ -153,10 +182,30 @@ def save_to_excel(dataframe, output_filename):
     return
 
 
-statements = "statements"
-pbar = tqdm(os.listdir(statements))
+def get_year_month(sheet_name):
+    year, month_name = sheet_name.split(' ')
+    month_dict = {
+        'JANUARI': 1, 'FEBRUARI': 2, 'MARET': 3, 'APRIL': 4,
+        'MEI': 5, 'JUNI': 6, 'JULI': 7, 'AGUSTUS': 8,
+        'SEPTEMBER': 9, 'OKTOBER': 10, 'NOVEMBER': 11, 'DESEMBER': 12
+    }
+    return int(year), month_dict[month_name]
+
+
+def reorder_sheets(output_filename):
+
+    wb = load_workbook(output_filename)
+    sheet_names = wb.sheetnames
+    sorted_sheets = sorted(sheet_names, key=get_year_month, reverse=True)
+    wb._sheets.sort(key=lambda x: sorted_sheets.index(x.title))
+    wb.save(output_filename)
+
+    return
+
+statements_folder = "statements"
+pbar = tqdm(os.listdir(statements_folder))
 for filename in pbar:
-    file_path = os.path.join(statements, filename)
+    file_path = os.path.join(statements_folder, filename)
     pbar.set_description("Processing %s" % filename)
 
     if os.path.isfile(file_path):
@@ -166,13 +215,21 @@ for filename in pbar:
         periode = header_dataframe.loc[header_dataframe[0] == 'PERIODE', 2].values[0]
         periode = ' '.join(reversed(periode.split()))
         no_rekening = header_dataframe.loc[header_dataframe[0] == 'NO. REKENING', 2].values[0]
+        output_filename = f'{no_rekening}.xlsx'
 
         dataframes = read_pdf(file_path, area = (251, 25, 805, 577), columns=[86, 184, 300, 340, 467], pages='all', pandas_options={'header': None, 'dtype': str}, force_subprocess=True)
+
+        init_balance = dataframes[0].loc[dataframes[0][1] == 'SALDO AWAL', 5].values[0]
+        init_balance = float(init_balance.replace(',', ''))
 
         df = union_source(dataframes)
         df = clean_numeric_columns(df, ['amount', 'balance'])
         df = insert_shifted_column(df)
 
         transaction_dataframe = extract_transactions(df)
+        transaction_dataframe = transaction_dataframe.drop('balance', axis=1)
+        transaction_dataframe = calculate_balance(transaction_dataframe)
 
-        save_to_excel(transaction_dataframe, f'{no_rekening}.xlsx')
+        save_to_excel(transaction_dataframe, output_filename)
+
+reorder_sheets(output_filename)
